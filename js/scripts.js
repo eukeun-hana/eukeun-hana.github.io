a1cafdc01280bdb68264cbc22f629411b0c67d70
eukeun-hana <connetflix@naver.com>
2025-12-28 21:31:56 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -98,54 +98,7 @@ function kakaoShare() {
 	}, 500);
 }
 
-function toggleFoldable() {
-    var content = document.querySelector('.foldable-content');
-    var toggleIcon = document.getElementById('toggleIcon');
-
-    // 펼치기/접기 상태 변경
-    if (content.style.display === 'none') {
-        content.style.display = 'block';
-        toggleIcon.innerText = '▲';
-        // 추가로 펼쳐진 상태에서 수행할 동작 추가 가능
-    } else {
-        content.style.display = 'none';
-        toggleIcon.innerText = '▼';
-        // 추가로 접힌 상태에서 수행할 동작 추가 가능
-    }
-}
-
-function toggleFoldable2() {
-    var content = document.querySelector('.foldable-content2');
-    var toggleIcon = document.getElementById('toggleIcon2');
-
-    // 펼치기/접기 상태 변경
-    if (content.style.display === 'none') {
-        content.style.display = 'block';
-        toggleIcon.innerText = '▲';
-        // 추가로 펼쳐진 상태에서 수행할 동작 추가 가능
-    } else {
-        content.style.display = 'none';
-        toggleIcon.innerText = '▼';
-        // 추가로 접힌 상태에서 수행할 동작 추가 가능
-    }
-}
-
-function toggleFoldable3() {
-    var content = document.querySelector('.foldable-content3');
-    var toggleIcon = document.getElementById('toggleIcon3');
-
-    // 펼치기/접기 상태 변경
-    if (content.style.display === 'none') {
-        content.style.display = 'block';
-        toggleIcon.innerText = '▲';
-        // 추가로 펼쳐진 상태에서 수행할 동작 추가 가능
-    } else {
-        content.style.display = 'none';
-        toggleIcon.innerText = '▼';
-        // 추가로 접힌 상태에서 수행할 동작 추가 가능
-    }
-}
-
+//카드 넘기기
 let current = 0;
 const cards = document.querySelectorAll(".card");
 
