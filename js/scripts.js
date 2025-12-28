bbffdb2af744978a067765793f896b62af097247
eukeun-hana <connetflix@naver.com>
2025-12-28 10:08:14 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -77,16 +77,16 @@ function kakaoShare() {
 			description: '2026.03.15',
 			imageUrl: 'https://github.com/eukeun-hana/eukeun-hana.github.io/blob/main/assets/img/main.jpg?raw=true',
         	link: {
-				mobileWebUrl: 'https://eukeun-hana.github.io/',
-				webUrl: 'https://eukeun-hana.github.io/',
+				mobileWebUrl: 'https://eukeun-hana.github.io/#!',
+				webUrl: 'https://eukeun-hana.github.io/#!',
 			},
 		},
         buttons: [
 	        {
 	            title: '모바일 청첩장 보기',
 	            link: {
-					mobileWebUrl: 'https://eukeun-hana.github.io/',
-					webUrl: 'https://eukeun-hana.github.io/',
+					mobileWebUrl: 'https://eukeun-hana.github.io/#!',
+					webUrl: 'https://eukeun-hana.github.io/#!',
 	            },
 			},
 		],
